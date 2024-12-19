706e61ed5d566b052c63df664c4477ce4ae68c59
Roger Oliveira <rogerpolvr@gmail.com>
2024-12-18 22:16:40 -0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
             "nikitabobko/tap/aerospace" # Tiling Window Manager
             "qbittorrent" # BitTorrent client
             "karabiner-elements"
+            "raycast"
           ];
           # onActivation.cleanup = "zap"; # Remove Homebrew packages not in the flake
           onActivation.autoUpdate = true; # Update Homebrew packages on activation
